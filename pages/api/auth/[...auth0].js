07a5fd24a1eb1937b713de641f896125e2524b93
Manikandan R <mani@example.com>
2024-10-20 20:51:02 +0530

    0Auth_Updated

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,8 @@
 import { handleAuth } from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+export default handleAuth({
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+    baseURL: process.env.AUTH0_BASE_URL,
+    clientID: process.env.AUTH0_CLIENT_ID,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  });
